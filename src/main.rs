e39031947a6d472ce765111178085c6a0bb9d6ba
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-06-11 12:10:44 +0200

    Add registers and PC

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 const MEMORY_SIZE: usize = 65536; /* 65536 locations */
 
+struct VM {
+    memory: [u16; MEMORY_SIZE],
+    registers: [u16; 10], /* 10 registers: R0-R7, COND and COUNT */
+    pc: u16,              /* program counter */
+}
+
+impl VM {
+    fn new() -> Self {
+        VM {
+            memory: [0; MEMORY_SIZE],
+            registers: [0; 10],
+            pc: 0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
