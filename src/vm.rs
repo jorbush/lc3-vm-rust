0b9111122f6d2a1203867cab708dea7d6f9db25e
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-06-11 13:09:01 +0200

    Refactor vm

---
+++ src/vm.rs
@@ -0,0 +1,17 @@
+const MEMORY_SIZE: usize = 65536; /* 65536 locations */
+
+struct VM {
+    memory: [u16; MEMORY_SIZE],
+    registers: [u16; 10],
+    pc: u16,
+}
+
+impl VM {
+    fn new() -> Self {
+        VM {
+            memory: [0; MEMORY_SIZE],
+            registers: [0; 10],
+            pc: 0,
+        }
+    }
+}
