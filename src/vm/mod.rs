09e15521c343d2c1f9871ad6144633c18d76f2c6
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-06-13 13:19:21 +0200

    Implement LDR

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -246,10 +246,16 @@ impl VM {
     }
 
     fn ldr(&mut self, instr: u16) {
-        todo!(
-            "{}",
-            format!("Instruction LDR ({:#x}) not implemented yet.", instr)
-        );
+        /*
+            15 14 13 12 | 11 10 9 | 8 7 6 | 5 4 3 2 1 0
+                0 1 1 0 |    DR   | BaseR | 6-bit offset
+        */
+        let dr = (instr >> 9) & 0x7;
+        let base_r = (instr >> 6) & 0x7;
+        let offset = Self::sign_extend(instr & 0x3F, 6);
+        let address = self.registers[base_r as usize].wrapping_add(offset);
+        self.registers[dr as usize] = self.memory[address as usize];
+        self.update_flags(dr as usize);
     }
 
     fn lea(&mut self, instr: u16) {
